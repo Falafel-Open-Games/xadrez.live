4e6bb6a2cba2cad15277d363a2447cd7d6e78bd6
Fabricio C Zuardi <fabricio@fabricio.org>
2026-06-22 08:30:16 -0300

    Render scheduled session status dynamically

---
+++ static/session-status.js
@@ -0,0 +1,51 @@
+(function () {
+  function localDateOnly(date) {
+    return new Date(date.getFullYear(), date.getMonth(), date.getDate());
+  }
+
+  function parseSessionDate(value) {
+    var match = /^(\d{4})-(\d{2})-(\d{2})$/.exec(value || "");
+    if (!match) {
+      return null;
+    }
+
+    return new Date(Number(match[1]), Number(match[2]) - 1, Number(match[3]));
+  }
+
+  function shortDate(date) {
+    return new Intl.DateTimeFormat("pt-BR", {
+      day: "2-digit",
+      month: "2-digit",
+    }).format(date);
+  }
+
+  function scheduledLabel(date, time) {
+    var today = localDateOnly(new Date());
+    var sessionDate = localDateOnly(date);
+    var diffDays = Math.round((sessionDate - today) / 86400000);
+
+    if (diffDays === 0) {
+      return "marcada para hoje às " + time;
+    }
+
+    if (diffDays === 1) {
+      return "marcada para amanhã às " + time;
+    }
+
+    return "marcada para " + shortDate(date) + " às " + time;
+  }
+
+  document.querySelectorAll("[data-session-status]").forEach(function (element) {
+    if (element.dataset.statusTone !== "scheduled") {
+      return;
+    }
+
+    var date = parseSessionDate(element.dataset.sessionDate);
+    var time = element.dataset.sessionTime;
+    if (!date || !time) {
+      return;
+    }
+
+    element.textContent = scheduledLabel(date, time);
+  });
+})();
